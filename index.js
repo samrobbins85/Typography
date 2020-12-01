3fe22626c688b2291d30a5f2bfbf4ff6d14240e3
Sam Robbins <samrobbinsgb@gmail.com>
2020-12-01 18:56:48 +0000

    Update to use Tailwind colours directly

--- index.js
+++ index.js
@@ -1,38 +1,41 @@
 const round = (num) =>
-	num
-		.toFixed(7)
-		.replace(/(\.[0-9]+?)0+$/, "$1")
-		.replace(/\.0$/, "");
+  num
+    .toFixed(7)
+    .replace(/(\.[0-9]+?)0+$/, "$1")
+    .replace(/\.0$/, "");
 const rem = (px) => `${round(px / 16)}rem`;
+const colors = require("tailwindcss/colors");
 module.exports = {
-	theme: {
-		extend: {
-			typography: (theme) => ({
-				DEFAULT: {
-					css: {
-						"code::after": {
-							content: "none",
-						},
-						"code::before": {
-							content: "none",
-						},
-						"pre code::after": {
-							content: "none",
-						},
-						code: {
-							backgroundColor: theme("colors.gray.200"),
-							color: theme("colors.gray.700"),
-							padding: "4px",
-							borderRadius: rem(2),
-						},
-						blockquote: {
-							quotes: "none",
-						},
-					},
-				},
-			}),
-		},
-	},
-	plugins: [require("@tailwindcss/typography")],
+  theme: {
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            "code::after": {
+              content: "none",
+            },
+            "code::before": {
+              content: "none",
+            },
+            "pre code::after": {
+              content: "none",
+            },
+            code: {
+              backgroundColor: colors.gray[200],
+              color: colors.gray[700],
+              padding: "4px",
+              borderRadius: rem(2),
+            },
+            blockquote: {
+              quotes: "none",
+            },
+            pre: {
+              backgroundColor: colors.coolGray[800],
+            },
+          },
+        },
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
 };
-
